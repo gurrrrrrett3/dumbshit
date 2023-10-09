3ba7a0862c6ab5492dcea70d14f05eff538738d5
Garrett Keneau <gart@gart.sh>
2023-10-08 20:09:55 -0400

    ratelimit

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,16 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-let currentUrl: string = "https://media.discordapp.net/attachments/1114694800042623007/1158229199023259689/image.png?ex=651b7c7f&is=651a2aff&hm=eb6d69c909e559173a9636d0a333b59f6d816ef3e5f96138fdfa0bc7acbf0b62&=&width=1158&height=900";
+let sockets: {
+  [id: string]: {
+    lastUrl: number;
+    requests: number;
+    uptime: number;
+  };
+};
+
+let currentUrl: string =
+  "https://media.discordapp.net/attachments/1114694800042623007/1158229199023259689/image.png?ex=651b7c7f&is=651a2aff&hm=eb6d69c909e559173a9636d0a333b59f6d816ef3e5f96138fdfa0bc7acbf0b62&=&width=1158&height=900";
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/client/index.html"));
@@ -24,6 +33,26 @@ io.on("connection", (socket) => {
   });
 
   socket.on("url", (url: string) => {
+    if (!sockets[socket.id]) {
+      sockets[socket.id] = {
+        lastUrl: Date.now(),
+        requests: 1,
+        uptime: 0,
+      };
+    }
+
+    if (Date.now() - sockets[socket.id].lastUrl < 10000) {
+      sockets[socket.id].requests++;
+    } else {
+      sockets[socket.id].requests = 1;
+    }
+
+    if (sockets[socket.id].requests > 10) {
+      socket.emit("error", "Too many requests");
+      console.log(`Rate limited ${socket.id}`);
+      return;
+    }
+
     console.log(`URL received: ${url}`);
     io.emit("url-send", url);
     currentUrl = url;
